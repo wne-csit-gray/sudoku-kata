306567c4fcaad74fe833712b954fac174aa0e5ff
Declan Gray <declan.gray-mullen@wne.edu>
2025-04-17 20:23:37 +0000

    test: all columns 1-9 digits

--- src/test/java/com/sudoku/BoardTests.java
+++ src/test/java/com/sudoku/BoardTests.java
@@ -2,6 +2,7 @@ package com.sudoku;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +13,26 @@ public class BoardTests {
         Board b = new Board(size);
         b.fill();
         checkAllPositionsNotNull(b);
+        checkAllRowsEachPositionAppearsOnce(b);
     }
 
     void checkAllPositionsNotNull(Board b){
         for (int i = 0; i < b.size; i++){
             for (int j = 0; j < b.size; j++){
                 assertNotNull(b.positionValue(i, j));
+            }
+        }
+    }
+
+    void checkAllRowsEachPositionAppearsOnce(Board b){
+        for (int j = 0; ;j < b.size; j++){
+            boolean[] found = new boolean[b.size];
+            for (int i = 0; i < b.size; i++){
+                int val = b.positionValue(i, j);
+                if (found[val]){
+                    assertTrue(false);
+                }
+                found[val] = true;
             }
         }
     }
