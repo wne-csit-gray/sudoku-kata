e04397684fd049d002c180e5846ebb94dd3a0ebd
Declan Gray <declan.gray-mullen@wne.edu>
2025-04-17 20:11:28 +0000

    test: createBoard fills all positions

--- src/test/java/com/sudoku/BoardTests.java
+++ src/test/java/com/sudoku/BoardTests.java
@@ -1,12 +1,21 @@
 package com.sudoku;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class BoardTests {
     @Test
     public void createBoard(){
-        Board b = new Board(9);
+        int size = 9;
+        Board b = new Board(size);
         b.fill();
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                assertNotEquals(null, b.positionValue(i, j));
+            }
+        }
+
     }
 }
