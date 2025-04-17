82cade08d44628af29df76b8396bed0a0d61e103
Declan Gray <declan.gray-mullen@wne.edu>
2025-04-17 19:47:31 +0000

    test: createBoard

---
+++ src/test/java/com/sudoku/BoardTests.java
@@ -0,0 +1,11 @@
+package com.sudoku;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class BoardTests {
+    @Test
+    public void createBoard(){
+        Board b = new Board();
+    }
+}
